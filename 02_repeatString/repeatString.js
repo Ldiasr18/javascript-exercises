814c7f6c44ce4cbc9b183912fa84e94ecdf3a09a
Lucas <ldiasr18@gmail.com>
2024-07-17 08:38:26 -0300

    exercises resolutions from 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat) {
+    if(repeat < 0){
+        return "ERROR";
+    }
+    let newString = "";
+    for (let i = 0; i < repeat; i++){
+        newString += word;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...extras) {
+    let newArray = [];
+    array.forEach(item => {
+        if (!extras.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
